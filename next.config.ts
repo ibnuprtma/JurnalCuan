363d6b640dabdbf06b92777681ad0aad18a7ecdc
Ibnu <pibnu86@gmail.com>
2026-08-21 16:05:43 +0700

    fix vercel build output config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   reactStrictMode: true,
   serverExternalPackages: ["@prisma/client", "pg"],
   images: {
